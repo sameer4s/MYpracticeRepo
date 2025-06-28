857f6ff4363851dde8cf5117bfc039a15480eee8
Sameer Shukla <137668159+sameer4s@users.noreply.github.com>
2025-06-28 22:22:48 +0545

    Add files via upload

---
+++ week_2/advance SQL/Exercise 5/code/Exercise 5.sql
@@ -0,0 +1,16 @@
+use EmployeeDB;
+go
+CREATE FUNCTION fn_CalculateAnnualSalary (@MonthlySalary DECIMAL(10,2))
+RETURNS DECIMAL(10,2)
+AS
+BEGIN
+    RETURN @MonthlySalary * 12
+END
+GO
+SELECT 
+    FirstName,
+    LastName,
+    Salary AS MonthlySalary,
+    dbo.fn_CalculateAnnualSalary(Salary) AS AnnualSalary
+FROM Employees
+WHERE EmployeeID = 1;
