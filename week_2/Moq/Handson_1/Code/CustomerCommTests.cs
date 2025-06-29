44508f6dcc2aba5b1e2bd5ff5e2b6cb6f7d1c7a8
Sameer Shukla <137668159+sameer4s@users.noreply.github.com>
2025-06-29 20:17:16 +0545

    Add files via upload

---
+++ week_2/Moq/Handson_1/Code/CustomerCommTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+namespace CustomerComm.Tests
+{
+    [TestFixture]
+    public class CustomerCommTests
+    {
+        private Mock<IMailSender> _mockMailSender;
+        private CustomerCommLib.CustomerComm _customerComm;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _mockMailSender = new Mock<IMailSender>();
+            _mockMailSender.Setup(m => m.SendMail(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+
+            _customerComm = new CustomerCommLib.CustomerComm(_mockMailSender.Object);
+        }
+
+        [Test]
+        public void SendMailToCustomer_ShouldReturnTrue_WhenMailSent()
+        {
+            var result = _customerComm.SendMailToCustomer();
+            Assert.IsTrue(result);  
+        }
+    }
+}
