54b16725af4ad313fb29afb2ac4ea928361d57e7
Sameer Shukla <137668159+sameer4s@users.noreply.github.com>
2025-07-06 10:27:24 +0530

    Add files via upload

---
+++ week3/RetrievingDatafromDatabase/code/Program.cs
@@ -0,0 +1,55 @@
+﻿using RetailInventory;
+using RetailInventory.Models;
+using Microsoft.EntityFrameworkCore;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using var context = new AppDbContext();
+
+        await context.Database.EnsureCreatedAsync();
+
+        // OPTIONAL: Clear old data (just for testing)
+        context.Products.RemoveRange(context.Products);
+        context.Categories.RemoveRange(context.Categories);
+        await context.SaveChangesAsync();
+
+        // Add categories
+        var electronics = new Category { Name = "Electronics" };
+        var groceries = new Category { Name = "Groceries" };
+
+        await context.Categories.AddRangeAsync(electronics, groceries);
+
+        // Add products
+        var product1 = new Product { Name = "Laptop", Price = 75000, Category = electronics };
+        var product2 = new Product { Name = "Rice Bag", Price = 1200, Category = groceries };
+
+        await context.Products.AddRangeAsync(product1, product2);
+        await context.SaveChangesAsync();
+
+        Console.WriteLine("Data inserted successfully!\n");
+
+        // Display all products
+        var products = await context.Products
+                                    .Include(p => p.Category)
+                                    .ToListAsync();
+
+        Console.WriteLine("Product List:");
+        foreach (var p in products)
+        {
+            Console.WriteLine($"{p.Name} - ₹{p.Price} ({p.Category.Name})");
+        }
+
+        var product = await context.Products.FindAsync(1);
+
+        Console.WriteLine($"\n Found by ID: {product?.Name ?? "Not Found"}");
+
+        var expensive = await context.Products.FirstOrDefaultAsync(p => p.Price > 50000);
+
+        Console.WriteLine($"\n Expensive Product: {expensive?.Name ?? "None"}");
+
+
+
+    }
+}
