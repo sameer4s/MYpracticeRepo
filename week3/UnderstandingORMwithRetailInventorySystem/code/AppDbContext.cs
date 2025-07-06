4de5dcd25fe1ca1d04888fb3fa374f6bf0fe0a4a
Sameer Shukla <137668159+sameer4s@users.noreply.github.com>
2025-07-06 09:48:09 +0530

    Add files via upload

---
+++ week3/UnderstandingORMwithRetailInventorySystem/code/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailInventory.Models;
+
+namespace RetailInventory
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=RetailInventoryDb;Trusted_Connection=True;");
+        }
+    }
+}
