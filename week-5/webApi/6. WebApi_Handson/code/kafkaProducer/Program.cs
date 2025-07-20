008ee6ec740382f2e249f38dd76789b960025ced
Sameer Shukla <137668159+sameer4s@users.noreply.github.com>
2025-07-21 00:25:54 +0530

    Add files via upload

---
+++ week-5/webApi/6. WebApi_Handson/code/kafkaProducer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+class Producer
+{
+    static async Task Main(string[] args)
+    {
+        var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+        using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+        Console.WriteLine("Producer started. Type messages:");
+
+        while (true)
+        {
+            Console.Write("You: ");
+            var message = Console.ReadLine();
+            await producer.ProduceAsync("chat-topic", new Message<Null, string> { Value = message });
+        }
+    }
+}
