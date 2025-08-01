6d13793664648aff446502ca425017996ba9b185
sameer4s <sameer000746@gmail.com>
2025-08-01 19:37:30 +0530

    Added cricketapp folder for week_7

---
+++ week_7/cricketapp/src/App.js
@@ -0,0 +1,18 @@
+// src/App.js
+import React, { useState } from 'react';
+import ListofPlayers from './components/ListofPlayers';
+import IndianPlayers from './components/IndianPlayers';
+
+function App() {
+  const [flag, setFlag] = useState(true); // Change to false to test other view
+
+  return (
+    <div className="App">
+      <h1> React Cricket App</h1>
+      <button onClick={() => setFlag(!flag)}>Toggle View</button>
+      {flag ? <ListofPlayers /> : <IndianPlayers />}
+    </div>
+  );
+}
+
+export default App;
